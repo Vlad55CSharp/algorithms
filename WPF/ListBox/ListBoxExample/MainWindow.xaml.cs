c69a6576f9cf7f2abfa8d563151a061d8e57b811
delphi2sharp@yandex.ru <Vlad55CSharp>
2020-11-30 22:15:55 +0600

    ListBox

--- WPF/ListBox/ListBoxExample/MainWindow.xaml.cs
+++ WPF/ListBox/ListBoxExample/MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace ListBoxExample
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void SimpleList_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+        
+            if (simpleList.SelectedItem != null)
+            {
+                ListBoxItem li = (ListBoxItem)simpleList.Items[simpleList.SelectedIndex];//получаем доступ к элементу
+                MessageBox.Show((string)li.Content);//получаем текст
+            }
+        }
+
+      
+        private void ExtendedList_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!(sender is ListBox))
+                return;
+            ListBox list = (ListBox)sender; //получаем ListBox            
+            ListBoxItem item = (ListBoxItem)list.Items[simpleList.SelectedIndex];//получаем доступ к выделенному элементу
+            StackPanel panel = (StackPanel)item.Content; //получаем доступ к панели
+            string result = ""; 
+            foreach (UIElement element in panel.Children)
+            {
+                if (element is TextBlock block) //проверяем, является ли текущий элемент панели текстовым блоком
+                {
+                    result += block.Text;
+                    result += ".";
+                }
+            }
+            _ = MessageBox.Show(result);
         }
     }
 }
