1856c1ce9e9684fd667fb77f9cce9120ee012863
delphi2sharp@yandex.ru <Vlad55CSharp>
2020-12-05 15:33:24 +0600

    Abstract

---
+++ AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass
+{
+    abstract class Person
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+
+        public Person(string name, string family)
+        {
+            Name = name;
+            Family = family;
+        }
+
+        abstract public string Display();
+    }
+}
---
+++ AbstractClass/AbstractClass/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass
+{
+    class Teacher: Person
+    {
+        public int Seniority { get; set; }
+        
+        public Teacher(string name, string family, int seniority) : base(name, family)
+        {
+            Seniority = seniority;
+        }
+
+        public override string Display()
+        {
+            return $"{Name} {Family} Стаж: {Seniority}";
+        }
+    }
+
+    
+}
