d9e50204ee0dfbad7fac8240183c4c2160b2e579
delphi2sharp@yandex.ru <Vlad55CSharp>
2021-08-25 21:39:01 +0600

    fix

---
+++ Permutations/Permutations/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число (N) от 1 до 255");
+            byte n = byte.Parse(Console.ReadLine());
+            
+            byte[] x = new byte[n];
+
+
+            void Swap(byte a, byte b)
+            {
+                byte c;
+                c = a;
+                a = b;
+                b = c;
+            }
+
+            void Permutation(byte[] perm, bool yes)
+            { 
+                int i = n - 2;
+                int j;
+                while ((i > -1) && perm[i - 1] > perm[i])
+                {
+                    i--;
+                }
+                if (i > 0)
+                {
+                    j = i+1;
+                    while ((j < n - 1) && (perm[j] > perm[i - 1]))
+                    {
+                        j++;
+                    }
+                    Swap(perm[i], perm[j]);
+                    for (int k = i; k <= (n + i) / 2; k++)
+                    {
+                        Swap(perm[k], perm[n - k + i]);
+                    }
+                }
+                else
+                    yes = false;
+            }
+
+            bool y = false;
+            for (byte m = 0; m <= n-1; m++)
+            {
+                x[m] = m;
+            }
+
+            do
+            {
+                for (int i = 0; i <= n-1; i++)
+                {
+                    Console.Write(x[i]);
+                }
+                Console.WriteLine();
+                Permutation(x, y);
+            }
+                while (!y);
+
+        }
+    }
+}
---
+++ SQLite/Example 1/SQLiteExample/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Data;
+
+namespace SQLiteExample
+{
+    class Program
+    {
+        static SQLiteConnection connection;
+        static SQLiteCommand command;
+        static SQLiteTransaction transaction;
+
+        static public bool Connect(string fileName)
+        {
+            try
+            {
+                connection = new SQLiteConnection("Data Source=" + fileName + ";Version=3; FailIfMissing=False");
+                connection.Open();
+                return true;
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine($"Ошибка доступа к базе данных. Исключение: {ex.Message}");
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (Connect("firstBase.sqlite"))
+            {
+                Console.WriteLine("Connected");
+                command = new SQLiteCommand(connection)
+                {
+                    CommandText = "CREATE TABLE IF NOT EXISTS [Person]([id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, [name] TEXT, [family] TEXT, [age] byte);"
+                };
+                
+                command.ExecuteNonQuery();
+                Console.WriteLine("Таблица создана");
+                Stopwatch sw = new Stopwatch();
+                sw.Restart();
+
+                
+                command.CommandText = "INSERT INTO Person (name, family, age) VALUES (:name, :family, :age)";
+                transaction = connection.BeginTransaction();//запускаем транзакцию
+                try
+                {
+                    for (int i = 1; i < 1001; i++)
+                    {
+                        command.Parameters.AddWithValue("name", "Сергей");
+                        command.Parameters.AddWithValue("family", "Петров");
+                        command.Parameters.AddWithValue("age", i);
+                        command.ExecuteNonQuery();
+                    }
+                    transaction.Commit(); //применяем изменения
+                    sw.Stop();
+                    Console.WriteLine(sw.Elapsed);
+                }
+                catch
+                {
+                    transaction.Rollback(); //откатываем изменения, если произошла ошибка
+                    throw; 
+                }
+            }
+        }
+    }
+}
